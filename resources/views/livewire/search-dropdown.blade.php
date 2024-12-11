06807755c28973b751c85f9a26e2687204609585
3bdallah <abdallahabdelhady777@gmail.com>
2024-12-12 01:40:37 +0200

    add alpinejs

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,5 +1,14 @@
-<div class="relative mt-3 md:mt-0">
-    <input wire:model.live.debounce.500ms="search" type="text"
+<div class="relative mt-3 md:mt-0" x-data="{ isOpen: true }" @click.away="isOpen = false">
+    <input wire:model.live.debounce.500ms="search" x-ref="search"
+        @keydown.window="
+   if (event.key === '/')
+    {
+        event.preventDefault();
+        $refs.search.focus();
+    }
+        "
+        @focus="isOpen = true" @keydown="isOpen = true" @keydown.escape.window="isOpen = false"
+        @keydown.shift.tab="isOpen = false" type="text"
         class="bg-gray-800 tx-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline"
         placeholder="Search" />
     <div class="absolute top-0">
@@ -10,13 +19,14 @@
     </div>
     <div wire:loading class="spinner top-0 right-0 mr-4 mt-3"></div>
     @if ($search)
-        <div class="absolute bg-gray-800 rounded text-sm w-64 mt-4">
+        <div class="absolute bg-gray-800 rounded text-sm w-64 mt-4" x-show.transition.opacity="isOpen">
             <ul>
                 @if ($movies->count())
                     @foreach ($movies as $movie)
                         <li class="border-b border-gray-700">
                             <a href="{{ route('movies.show', $movie->id) }}"
-                                class="block hover:bg-gray-700 px-3 py-3 flex items-center">
+                                class="block hover:bg-gray-700 px-3 py-3 flex items-center"
+                                @if ($loop->last) @keydown.tab="isOpen = false" @endif>
                                 @if ($movie->posterPath)
                                     <img src="{{ $movie->searchPoster }}" alt="poster" class="w-8">
                                 @else
